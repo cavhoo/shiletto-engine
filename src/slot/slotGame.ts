8e68cba3c3d44407fdcfae8cbedfb5369ed1e8b8
Hendrik Müller-Röhr <darksolj@live.com>
2022-11-03 21:25:26 +0100

    Add slotgame abstract class, fix build directory

--- src/slot/slotGame.ts
+++ src/slot/slotGame.ts
@@ -1,10 +1,24 @@
 import { Game } from "../core";
+import { Entity } from "../rendering";
 
-export class SlotGame extends Game {
+export abstract class SlotGame extends Game {
+  protected reelContainer!: Entity;
+  protected background!: Entity;
+
   constructor() {
     super({
       name: "slot",
       size: [1920, 1080],
     });
   }
+
+  protected abstract createReelContainer(): void;
+
+  protected abstract createLineMarkers(): void;
+
+  protected abstract createPaylines(): void;
+
+  protected abstract createBackground(): void;
+
+  protected abstract createSymbols(): void;
 }
